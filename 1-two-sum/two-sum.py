9e703732ea92297dba30e745695c2da37c9eebb6
N J <55899982+Boss679@users.noreply.github.com>
2024-03-30 15:31:04 -0400

    Time: 53 ms (88.03%) | Memory: 17.8 MB (41.49%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,26 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        Finds the indices of two numbers in the input list that add up to the target value.
+
+        Args:
+            nums (List[int]): The list of integers.
+            target (int): The target sum.
+
+        Returns:
+            List[int]: A list containing the indices of the two numbers that sum up to the 
+            target.
+        """
+        
+        # Dictionary to store previously seen numbers and their indices.
         prevMap = {}  # val -> index
 
+        # Iterate through the list of numbers along with their indices.
         for i, n in enumerate(nums):
+            # Calculate the difference needed to reach the target sum.
             diff = target - n
+            # If the difference is found in the dictionary, return the two numbers' indices.
             if diff in prevMap:
                 return [prevMap[diff], i]
+            # Otherwise, store the current number and its index in the dictionary.
             prevMap[n] = i
