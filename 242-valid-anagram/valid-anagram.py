a3ba5901ffceec5e8423e8eaf58b2af3ccc7c706
N J <55899982+Boss679@users.noreply.github.com>
2024-03-30 00:14:12 -0400

    Time: 49 ms (59.35%) | Memory: 17 MB (34.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,11 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        """
+        Determines if two input strings are anagrams of each other.
+
+        Args:
+            s (str): The first input string.
+            t (str): The second input string.
+
+        Returns:
+            bool: True if the strings are anagrams, False otherwise.
+        """
+        
         if len(s) != len(t):
             return False
 
+        # Create dictionaries to store the count of each character in both strings.
         countS, countT = {}, {}
 
+        # Iterate through each character in the strings and update their counts in the  dictionaries.
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        # If the character counts in both dictionaries are equal, the strings are anagrams.
         return countS == countT
